8967de37cc51ba7feb595f3447b47d8a5d8fca97
Oni01110011 <aichingerganas.bernhard@gmail.com>
2024-10-22 22:55:01 +0200

    Implemented Method: DirectoryContents.startWatching Also Commented: DirectoryContents.attach, DirectoryContents.detach, DirectoryContents.notifyChange, DirectoryContents.updateFileCounts and DirectoryContents.getFileExtension

--- src/DirectoryContents.java
+++ src/DirectoryContents.java
@@ -12,21 +12,36 @@ public class DirectoryContents {
         updateFileCounts();
     }
 
+    /**
+     * Methode zum Hinzufügen eines Observers, der Observer wird
+     * sofort mit den aktuellen Dateiinformationen "versorgt"
+     * @param observer Der an dem Path anzuhängende Observer
+     */
     public void attach(FileObserver observer) {
         observers.add(observer);
         observer.update(fileCounts);
     }
 
+    /**
+     * Methode zum Entfernen eines Observers
+     * @param observer Der zu entfernende Observer
+     */
     public void detach(FileObserver observer) {
         observers.remove(observer);
     }
 
-    private void notifyChange() {
+    /**
+     * Benachrichtige alle Observer über eine Änderung
+     */
+    private void notifyChanges() {
         for (FileObserver observer : observers) {
             observer.update(fileCounts);
         }
     }
 
+    /**
+     * Aktualisiere die Datei-Typ Zählung und benachrichtige die Observer
+     */
     public void updateFileCounts() {
         try {
             fileCounts.clear();
@@ -35,16 +50,48 @@ public class DirectoryContents {
                 String fileExtension = getFileExtension(fileName);
                 fileCounts.put(fileExtension, fileCounts.getOrDefault(fileExtension, 0) + 1);
             });
-            notifyChange();
+            notifyChanges();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     *  Extrahiert die Dateiendung
+     * @param fileName der Name der File deren Dateiendung zu extrahieren ist
+     * @return die Dateiendung
+     */
     private String getFileExtension(String fileName) {
         int dotIndex = fileName.lastIndexOf('.');
         return (dotIndex == -1) ? "Unknown" : fileName.substring(dotIndex + 1);
     }
 
+    /**
+     * Methode, um den Ordner zu überwachen und bei Änderungen zu reagieren
+     * @throws IOException Sollte das Verzeichnis nicht exestieren
+     * @throws InterruptedException
+     */
+    public void startWatching() throws IOException, InterruptedException {
+        WatchService watchService = FileSystems.getDefault().newWatchService();
+        dir.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY);
 
+        System.out.println("Watcher gestartet. Überwacht: " + dir.toString());
+
+        while (true) {
+            WatchKey key = watchService.take();
+
+            for (WatchEvent<?> event : key.pollEvents()) {
+                WatchEvent.Kind<?> kind = event.kind();
+                Path filePath = (Path) event.context();
+
+                System.out.println("Ereignis: " + kind.name() + " - " + filePath);
+                updateFileCounts();
+            }
+
+            boolean valid = key.reset();
+            if (!valid) {
+                break;
+            }
+        }
+    }
 }
