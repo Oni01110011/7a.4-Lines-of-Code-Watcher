d0cae6340702b8bb4fbfc19d5b10d67989cc804f
Oni01110011 <aichingerganas.bernhard@gmail.com>
2024-10-22 23:33:37 +0200

    Updated Method: FileObserver.update for countLines

--- src/FileObserver.java
+++ src/FileObserver.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 
 public interface FileObserver {
-    void update(Map<String, Integer> fileCounts);
+    void update(Map<String, Integer> fileCounts, Map<String, int[]> lineCounts);
+
 }
