e16c311da06e51ca411f0fe54bc0d237ab976755
Oni01110011 <aichingerganas.bernhard@gmail.com>
2024-10-22 22:16:03 +0200

    Override the FileWatcher.update Method

--- src/FileWatcher.java
+++ src/FileWatcher.java
@@ -1,2 +1,13 @@
-public class FileWatcher {
+import java.util.Map;
+
+public class FileWatcher implements FileObserver {
+
+    @Override
+    public void update(Map<String, Integer> fileCounts) {
+        System.out.println("\nDateien:");
+        System.out.println("-------------------");
+        fileCounts.forEach((fileType, count) -> {
+            System.out.printf("%-15s %d%n", fileType, count);
+        });
+    }
 }
