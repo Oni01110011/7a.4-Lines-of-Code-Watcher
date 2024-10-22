4b5ce1ca5bfceada1c5ca120753f470743bfd848
Oni01110011 <aichingerganas.bernhard@gmail.com>
2024-10-22 23:34:29 +0200

    Revamped Override Method: for countLines

--- src/FileWatcher.java
+++ src/FileWatcher.java
@@ -1,17 +1,19 @@
-import java.io.File;
 import java.util.Map;
-import java.util.Observable;
-import java.util.Observer;
 
 public class FileWatcher implements FileObserver {
 
     @Override
-    public void update(Map<String, Integer> fileCounts) {
-        System.out.println("\nDateien");
-        System.out.println("-------------------");
+    public void update(Map<String, Integer> fileCounts, Map<String, int[]> lineCounts) {
+        System.out.println("\nDateien  Zeilen  Davon Code  Davon Leerzeilen");
+        System.out.println("---------------------------------------------------------");
+
         fileCounts.forEach((fileType, count) -> {
-            System.out.printf("%-15s %d%n", fileType, count);
+            int[] lineData = lineCounts.getOrDefault(fileType, new int[]{0, 0, 0});
+            int totalLines = lineData[0];
+            int codeLines = lineData[1];
+            int emptyLines = lineData[2];
+
+            System.out.printf("%-12s %6d %10d %15d %18d%n", fileType, count, totalLines, codeLines, emptyLines);
         });
-        System.out.println("-------------------");
     }
 }
