a412150035a12dce7539b9a36b1f2ed818db4416
Oni01110011 <aichingerganas.bernhard@gmail.com>
2024-10-22 22:49:43 +0200

    Implemented Method: DirectoryContents.startWatching    Also Commented: DirectoryContents.attach, DirectoryContents.detach, DirectoryContents.notifyChange, DirectoryContents.updateFileCounts and DirectoryContents.getFileExtension

--- src/FileWatcher.java
+++ src/FileWatcher.java
@@ -1,13 +1,17 @@
+import java.io.File;
 import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
 
 public class FileWatcher implements FileObserver {
 
     @Override
     public void update(Map<String, Integer> fileCounts) {
-        System.out.println("\nDateien:");
+        System.out.println("\nDateien");
         System.out.println("-------------------");
         fileCounts.forEach((fileType, count) -> {
             System.out.printf("%-15s %d%n", fileType, count);
         });
+        System.out.println("-------------------");
     }
 }
